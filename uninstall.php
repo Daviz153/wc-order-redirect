374101d2fcca726c78e10e968d53712c7ae2438e
Daviz153 <135850119+Daviz153@users.noreply.github.com>
2026-06-09 15:37:37 +0900

    fix: uninstall에서 HPOS 주문 메타(_wcor_redirected) 누락 삭제 추가

--- uninstall.php
+++ uninstall.php
@@ -8,3 +8,10 @@ delete_post_meta_by_key('_wc_order_redirect_url');
 delete_option('wcor_enabled');
 delete_option('wcor_default_url');
 delete_option('wcor_redirect_log');
+
+// HPOS 주문 메타 삭제 (legacy postmeta는 위 delete_post_meta_by_key로 처리됨)
+global $wpdb;
+$hpos_table = $wpdb->prefix . 'wc_orders_meta';
+if ($wpdb->get_var($wpdb->prepare('SHOW TABLES LIKE %s', $hpos_table)) === $hpos_table) {
+    $wpdb->delete($hpos_table, ['meta_key' => '_wcor_redirected'], ['%s']);
+}
